73920246e9eea34e9948409494d73767ad6e1489
Mahejabeen Mahiboob Shaikh <103279160+MahejabeenShaikh@users.noreply.github.com>
2022-10-01 19:28:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-const addBtn = document.getElementById('add')  
-const notes = JSON.parse(localStorage.getItem('notes'))  
-if(notes) {  
-  notes.forEach(note => addNewNote(note))  
-}  
-addBtn.addEventListener('click', () => addNewNote())  
-function addNewNote(text = '') {  
-  const note = document.createElement('div')  
-  note.classList.add('note')  
-  note.innerHTML = `  
-  <div class="tools">  
-    <button class="edit"><i class="fas fa-edit"></i></button>  
-    <button class="delete"><i class="fas fa-trash-alt"></i></button>  
-  </div>  
-  <div class="main ${text ? "" : "hidden"}"></div>  
-  <textarea class="${text ? "hidden" : ""}"></textarea>  
-  `  
-  const editBtn = note.querySelector('.edit')  
-  const deleteBtn = note.querySelector('.delete')  
-  const main = note.querySelector('.main')  
-  const textArea = note.querySelector('textarea')  
-  textArea.value = text  
-  main.innerHTML = marked(text)  
-  deleteBtn.addEventListener('click', () => {  
-    note.remove()  
-    updateLS()  
-  })  
-  editBtn.addEventListener('click', () => {  
-    main.classList.toggle('hidden')  
-    textArea.classList.toggle('hidden')  
-  })  
-  textArea.addEventListener('input', (e) => {  
-    const { value } = e.target  
-    main.innerHTML = marked(value)  
-    updateLS()  
-  })  
-  document.body.appendChild(note)  
-}  
-function updateLS() {  
-  const notesText = document.querySelectorAll('textarea')  
-  const notes = []  
-  notesText.forEach(note => notes.push(note.value))  
-  localStorage.setItem('notes', JSON.stringify(notes))  
+function main () {
+  var height = parseFloat(document.getElementById('Height').value);
+  var weight = parseFloat(document.getElementById('Weight').value);
+
+  var imc = weight / (height/100)**2;
+  imc = imc.toFixed(2);
+
+  if (imc > 0 && imc <= 15) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>very severely underweight</span>.';
+  } else if (imc > 15 && imc <= 16) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>severely underweight</span>.';
+  } else if (imc > 16 && imc <= 18.5) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Underweight</span>.';
+  } else if (imc > 18.5 && imc <= 25) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Normal (healthy weight)</span>.';
+  } else if (imc > 25 && imc <= 30) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Overweight</span>.';
+  } else if (imc > 30 && imc <= 35) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Obese Class I (Moderately obese)</span>.';
+  } else if (imc > 35 && imc <= 40) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Obese Class II (Severely obese)</span>.';
+  } else if (imc > 40 && imc <= 45) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Obese Class III (Very severely obese)</span>.';
+  } else if (imc > 45 && imc <= 50) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Obese Class IV (Morbidly Obese)</span>.';
+  } else if (imc > 50 && imc <= 60) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Obese Class V (Super Obese)</span>.';
+  } else if (imc > 60) {
+    document.getElementById('output').innerHTML = 'Your BMI is: ' + imc + '<br>Your weight is <span>Obese Class VI (Hyper Obese)</span>.';
+  } else {
+    document.getElementById('output').innerHTML = 'Please type valid numbers.';
+  }
 }
